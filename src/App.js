f772c31fc6c0a7115f0bee6fe2095515338b306b
Rishi Karthikeyan <rishikarthikeyan@Rishis-MacBook-Air-2.local>
2022-01-26 22:43:25 +0400

    Task toggle reminder

--- src/App.js
+++ src/App.js
@@ -23,15 +23,28 @@ function App() {
     },
   ]);
 
-  // Delete task
+  // Delete Task
   const deleteTask = (id) => {
-    console.log('Deleted : ', id);
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
   };
 
   return (
     <div className='container'>
       <Header title='Task Tracker' />
-      <Tasks tasks={tasks} onDelete={deleteTask} />
+      {tasks.length != 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        'No tasks to Show'
+      )}
     </div>
   );
 }
